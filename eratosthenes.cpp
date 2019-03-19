4348bf2dde0e9c9026ed9ddc704a8d604593549b
daiki.oka1118 <hibari235711@gmail.com>
2019-03-19 16:16:47 +0900

    add eratosthenes sieve

---
+++ eratosthenes.cpp
@@ -0,0 +1,16 @@
+vector<int> prime;
+vector<bool> is_prime;
+int eratosthenes(int k){
+    is_prime.resize(k+1, 1);
+    is_prime[0] = false;
+    is_prime[1] = false;
+    for(int i=2; i<=k; i++){
+        if(is_prime[i]){
+            prime.push_back(i);
+            for(int j=2*i; j<=k; j+=i){
+                is_prime[j] = false;
+            }
+        }
+    }
+    return (int)prime.size();
+}
